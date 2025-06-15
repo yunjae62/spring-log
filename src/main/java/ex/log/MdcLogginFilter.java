ebc7898fb995d66b5da4f2748a24071b3a3beb8d
yunjae62 <wodbs7758@naver.com>
2025-06-15 14:42:29 +0900

    feat: HTTP 요청에 대한 MDC 필터 추가

---
+++ src/main/java/ex/log/MdcLogginFilter.java
@@ -0,0 +1,30 @@
+package ex.log;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class MdcLogginFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain filterChain
+    ) throws ServletException, IOException {
+        try {
+            String requestId = UUID.randomUUID().toString().substring(0, 8);
+            MDC.put("requestId", requestId);
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.clear();
+        }
+    }
+}
